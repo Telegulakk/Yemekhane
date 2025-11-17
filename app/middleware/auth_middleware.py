8f39414e4c87b5984937ebbf2772f34d9393c86f
Telegulakk <huseyinbuyuk313@gmail.com>
2025-11-17 23:24:22 +0300

    current_user_or_test() eklendi

--- app/middleware/auth_middleware.py
+++ app/middleware/auth_middleware.py
@@ -1,4 +1,14 @@
 from functools import wraps
+import os
+from flask_jwt_extended import current_user
+
+TEST_USER_ID = os.getenv("TEST_USER_ID", None)
+
+def current_user_or_test():
+    if TEST_USER_ID:  #loginsiz test girişi (.env ye databaseden seçtğimiz bir user idsini koyarız)
+        return TEST_USER_ID # böylece istediğimiz user idsi döner
+    return current_user.id #aksi halde login ile giriş yapmış user arayacak ve onun idsini bulucak (normalde olması gereken)
+
 
 def token_required(fn):
     """Geçici olarak devre dışı"""
@@ -8,10 +18,10 @@ def student_required(fn):
     """Geçici olarak devre dışı"""
     return fn
 
-def get_current_user():
-    """Geçici olarak None döner"""
-    return None
 
+
+
+# en en en son vakit kalırsa eklenecek
 def admin_required(fn):
     """Geçici olarak devre dışı"""
     return fn
